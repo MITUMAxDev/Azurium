da68c9fa1a6c6e2bf48d2fb52062dcf72fa457c8
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2024-10-06 17:20:05 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,11 @@
-local placeId = tonumber(game.PlaceId)
+local placeId = tostring(game.PlaceId)
 
-if placeId == "" then
-
+if placeId == "10704789056" then
+  -- DRIVE WORLD
+  loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/MITUMAxDev/Azurium/main/maps/driveworld'))()
 elseif placeId == "" then
-
+  -- RIVALS
+  
 else
   game:GetService("Players").LocalPlayer:Kick("Unsupported Map/แมพไม่รองรับ")
 end
