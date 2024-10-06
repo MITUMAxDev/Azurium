7deaefb8b2bbdcca643cb115d7df39cf4dbacbd4
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2024-10-06 17:12:47 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local placeId = tonumber(game.PlaceId)
+
+if placeId == "" then
+
+elseif placeId == "" then
+
+else
+  game:GetService("Players").LocalPlayer:Kick("Unsupported Map/แมพไม่รองรับ")
+end
