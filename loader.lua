f49207591ad09a02e5b58b53ab074db0db6708e1
ntopy <164611609+ntopy@users.noreply.github.com>
2024-10-07 13:48:47 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,8 +3,9 @@ local placeId = tostring(game.PlaceId)
 if placeId == "10704789056" then
   -- DRIVE WORLD
   loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/MITUMAxDev/Azurium/main/maps/driveworld'))()
-elseif placeId == "" then
+elseif placeId == "17625359962" then
   -- RIVALS
+  loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/MITUMAxDev/Azurium/refs/heads/main/maps/Rivals'))()
   
 else
   game:GetService("Players").LocalPlayer:Kick("Unsupported Map/แมพไม่รองรับ")
