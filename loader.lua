b87697369fa955ac7a2095fd01e18af48b9c6f2f
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2024-10-10 14:02:12 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,11 @@ if placeId == "10704789056" then
 elseif placeId == "17625359962" then
   -- RIVALS
   loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/MITUMAxDev/Azurium/refs/heads/main/maps/Rivals'))()
-  
+
+elseif placeId == "" then
+  -- STAND UPRIGHT
+  loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/MITUMAxDev/Azurium/refs/heads/main/maps/Stand-Upright'))()
+
 else
   game:GetService("Players").LocalPlayer:Kick("Unsupported Map/แมพไม่รองรับ")
 end
