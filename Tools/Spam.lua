6cb75842a14d224aaff136b71a54def7a0bb7f48
EclipseVSC <166280522+EclipseVSC@users.noreply.github.com>
2024-10-19 19:18:14 +0000

    Update Spam.lua

--- Tools/Spam.lua
+++ Tools/Spam.lua
@@ -1,1 +1,22 @@
+local TargetPlace = "8540168650"
+local TargetJob = "78ae31e2-8559-4421-8f58-b04b6e66f64e"
+local TargetPlayer = "TENN999FF"
+---- [[ TELEPORT ]] ----
+local thisPlace = tostring(game.PlaceId)
+local thisJob = tostring(game.JobId)
+local thisPlayer = tostring(game:GetService("Players").LocalPlayer.Name)
+local function gotoServer(place, job)
+local success = pcall(function()
+  game:GetService("TeleportService"):TeleportToPlaceInstance(place, job, game.Players.LocalPlayer)
+  end)
 
+if not success then
+wait(1)
+gotoServer(place, job)
+end
+end
+
+---- [[ Check ]] ----
+if TargetPlayer == thisPlayer and TargetPlace ~= thisPlace or TargetJob ~= thisJob then
+gotoServer(TargetPlace, TargetJob)
+end
