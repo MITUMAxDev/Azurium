53a568ef5f8d8183372cf5063163f44405c57175
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2024-10-20 07:39:11 +0700

    Update Spam.lua

--- Tools/Spam.lua
+++ Tools/Spam.lua
@@ -1,1 +1,25 @@
+local TargetPlace = "8540168650"
+local TargetJob = "3065c677-3e08-4792-9c64-da4ea5a05bb3"
+local TargetPlayer = "1xlrz163"
+---- [[ TELEPORT ]] ----
+local thisPlace = tostring(game.PlaceId)
+local thisJob = tostring(game.JobId)
+local thisPlayer = tostring(game:GetService("Players").LocalPlayer.Name)
 
+local function gotoServer(place, job)
+local success = pcall(function()
+  game:GetService("TeleportService"):TeleportToPlaceInstance(place, job, game.Players.LocalPlayer)
+  end)
+
+if not success then
+wait(1)
+gotoServer(place, job)
+end
+end
+
+---- [[ Check ]] ----
+if TargetPlayer == thisPlayer then -- Only checks if the local player matches the TargetPlayer
+if TargetPlace ~= thisPlace or TargetJob ~= thisJob then
+gotoServer(TargetPlace, TargetJob)
+end
+end
